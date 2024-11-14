f184cdb22fe8b51802d256b4c7d5c0f9a23bc937
Leegiyeon <rldus3512@naver.com>
2024-11-14 20:01:13 +0900

    remove: 함수 인자 삭제

--- tests/test_api_handler.py
+++ tests/test_api_handler.py
@@ -3,7 +3,7 @@ from utils.api_handler import fetch_financial_products
 
 class TestAPIHandler(unittest.TestCase):
     def test_fetch_financial_products(self):
-        result = fetch_financial_products("Savings account with high interest")
+        result = fetch_financial_products()
         self.assertIsInstance(result, list)
 
 if __name__ == "__main__":
