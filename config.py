ea923095781e9648a502b6644751fa415ee77a2d
Leegiyeon <rldus3512@naver.com>
2024-11-14 19:54:43 +0900

    fix: FSS_API_KEY 추가

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MAKE_API_KEY = os.getenv("MAKE_API_KEY")
+FSS_API_KEY = os.getenv("FSS_API_KEY") 
 
 # API 관련 기본 설정
 BASE_URL = "https://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json"
